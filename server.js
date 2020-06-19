a5e6531b60faecb33192fc25b03f3a656de138a6
Richard Russell <9446579+RussellAjax@users.noreply.github.com>
2020-06-19 10:38:44 +0700

    Created HTTP request routes and methods

--- server.js
+++ server.js
@@ -16,6 +16,20 @@ app.use(bodyParser.json());
 //Parse requests of content type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended:true}));
 
+/*Connect to Mongoose Database*/
+const db = require('./app/models');
+db.mongoose
+	.connect(db.url, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	})
+	.then(() => {
+		console.log("Connected to Mongo Database!");
+	})
+	.catch(err => {
+		console.log("Cannot connect to the database!", err);
+	});
+
 /*Create default route*/
 app.get('/', (req, res) => {
 	res.json({message: 'Welcome to the REST API'}); 
